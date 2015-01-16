7c619d6e9178bffca8c8dfe922656c47af335a5e
lorenzosaraiva <lorenzo.saraiva@hotmail.com>
2015-01-16 14:17:39 -0200

    Initial Commit

---
+++ Orbilis/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  Orbilis
+//
+
+//  Copyright (c) 2015 BEPID-PucRJ. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ Orbilis/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  Orbilis
+//
+
+//  Copyright (c) 2015 BEPID-PucRJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
